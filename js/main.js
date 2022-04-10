ef345fa3601d0946449989614458f2eb62bf9702
SaharMotlagh <s_mahmoudimotlagh@fanshaweonline.ca>
2022-04-10 14:35:32 -0400

    adding files and folders

---
+++ js/main.js
@@ -0,0 +1,40 @@
+var a;
+function show_hide()
+{
+    if(a==1)
+    {
+        document.getElementById("image").style.display="inline";
+
+        return a=0;
+    }
+
+    else
+    {
+        document.getElementById("image").style.display="none";
+        return a=1;
+    }
+
+}
+
+
+
+function show_now()
+{
+    if(a==1)
+    {
+        document.getElementById("imageN2").style.display="inline";
+
+        return a=0;
+    }
+
+    else
+    {
+        document.getElementById("imageN2").style.display="none";
+        return a=1;
+    }
+
+}
+
+
+
+
